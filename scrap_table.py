c9249f755d306fd88256783302aa475806a1e4e4
Jordinn Martin Reyes Medina <113043632+jordinnreyes@users.noreply.github.com>
2024-11-14 02:47:53 -0500

    Update scrap_table.py

--- scrap_table.py
+++ scrap_table.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import boto3
 import uuid
+import json  # Importar json para imprimir datos de manera legible
 
 def lambda_handler(event, context):
     # URL de la página web que contiene los sismos reportados
     url = "https://www.igp.gob.pe/servicios/centro-sismologico-nacional/ultimo-sismo/sismos-reportados"
     
-
     # Realizar la solicitud HTTP a la página web
     response = requests.get(url)
     if response.status_code != 200:
@@ -36,6 +36,10 @@ def lambda_handler(event, context):
         cells = row.find_all('td')
         if len(cells) == len(headers):  # Verifica que cada fila tenga todas las celdas necesarias
             rows.append({headers[i]: cells[i].text.strip() for i in range(len(headers))})
+
+    # Depuración: imprimir los datos extraídos
+    print(json.dumps(rows, indent=4))  # Muestra las filas extraídas en formato legible
+    print(f"Se encontraron {len(rows)} filas de datos")  # Muestra el número de filas encontradas
 
     # Guardar los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
