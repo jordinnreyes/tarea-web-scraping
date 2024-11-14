dc128044408882fdd97eff3a95059572ceaa0817
Jordinn Martin Reyes Medina <113043632+jordinnreyes@users.noreply.github.com>
2024-11-14 02:35:44 -0500

    Update scrap_table.py

--- scrap_table.py
+++ scrap_table.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
 
     # Guardar los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('TablaWebScrapping')
+    table = dynamodb.Table('TablaWebScrapping2')
 
     # Eliminar todos los elementos de la tabla antes de agregar los nuevos
     scan = table.scan()
