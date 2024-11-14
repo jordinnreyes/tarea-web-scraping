fa0fb7431ca14cce6c1d56c7ba00c55f3ebef7c4
Jordinn Martin Reyes Medina <113043632+jordinnreyes@users.noreply.github.com>
2024-11-14 03:23:06 -0500

    Update scrap_table.py

--- scrap_table.py
+++ scrap_table.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
 
     # Guardar los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('WeatherData')
+    table = dynamodb.Table('TablaIMDBTopMovies')
 
     # Eliminar todos los elementos de la tabla antes de agregar los nuevos
     scan = table.scan()
